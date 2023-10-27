389740abd0fe1d435e5f338edc7830420ff43e78
Ivan Leow <95156673+ivanleow2000@users.noreply.github.com>
2023-10-28 09:32:16 +1100

    Add files via upload

--- Modelling/RF script Ivan.R
+++ Modelling/RF script Ivan.R
@@ -12,8 +12,8 @@
 #11. | Use typical tree model stopping criteria to determine when a tree is complete (but do not prune)
 #12. end
 
-# Advantages: lower variance, uses decollerated trees unlike bagging
-# Disadvantages: Low interpretability, unable to discern effects of predictors
+# Advantages: lower variance, high accuracy, good in large datasets, uses decollerated trees unlike bagging so does not overfit
+# Disadvantages: Low interpretability, computationally expensive, time consuming
 
 # Script start
 # Packages
@@ -32,6 +32,8 @@ data <- read.csv("Data/Data.csv")
 summary(data)
 
 # Clean data
+data["FWI_99th_flag"][data["FWI_99th_flag"] == TRUE] <- 1 #Convert T/F to 1/0 for easier model handling
+data["FWI_99th_flag"][data["FWI_99th_flag"] == FALSE] <- 0 #^
 data <- na.omit(data) # Remove NA values
 data <- data[-c(1)] # Remove date column
 
@@ -40,23 +42,32 @@ split <- sample.split(data, SplitRatio = 0.8)
 data_train <- subset(data, split == "TRUE") 
 data_test <- subset(data, split == "FALSE") 
 
-data$Bushfire_Flag <- as.factor(data$Bushfire_Flag)    
+data$Bushfire_Flag <- as.factor(data$Bushfire_Flag) 
 data_train$Bushfire_Flag <- as.factor(data_train$Bushfire_Flag)
 data_test$Bushfire_Flag <- as.factor(data_test$Bushfire_Flag)
 
 # Look for best mtry
-#bestmtry <- tuneRF(data_train[-c(24)], data_train$Bushfire_Flag, ntreeTry = 500,
+#bestmtry <- tuneRF(data_train[-c(13)], data_train$Bushfire_Flag, ntreeTry = 500,
 #                   mtryStart  = 5,stepFactor = 1.2, improve = 0.01, trace=T) 
-bestmtry <- 14
+bestmtry <- 8
+#14 using old data, 8 using new data
 
 # Create RF model
 model <- randomForest(Bushfire_Flag~.,data= data_train, mtry=bestmtry)
+model #summary
+importance(model) # shows how much each factor contributes to GINI index (error) decrease
 
-importance(model) 
+# Prediction
+pred_test <- predict(model, newdata = data_test, type= "vote")
+pred_test <- as.data.frame(pred_test) #convert to df
+colnames(pred_test)[1] = "prediction" #rename column
 
-# ROC/AUC
-pred_test <- predict(model, newdata = data_test, type= "class")
+# Re-order soft delete (no need to reorder since both datasets are in the same jumbled up order)
+#pred_test$index <- as.numeric(row.names(pred_test))
+#df <- as.data.frame(pred_test[order(pred_test$index), ]$prediction)
+#colnames(df)[1] = "prediction"
 
-roc_test<- roc(data_test$Bushfire_Flag, pred_test)
-auc(roc.test)
-
+# ROC
+roc_test<- roc(data_test$Bushfire_Flag, pred_test$prediction)
+roc_test # summary and AUC
+plot(roc_test) #ROC curve
