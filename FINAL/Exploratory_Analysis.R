d24e44ac9d3169716110bdf9ee96e12e74744392
cs9418 <113615250+cs9418@users.noreply.github.com>
2023-11-10 20:56:17 +1100

    Add files via upload

---
+++ FINAL/Exploratory_Analysis.R
@@ -0,0 +1,77 @@
+## Set working directory
+# setwd()
+
+## Initialise packages
+# install.packages("pacman")
+pacman::p_load("tidyverse")
+
+## Import dataset
+data <- read.csv("df.csv")
+
+## Fix variable formats
+data$Date <- ymd(data$Date)
+data$State <- as.factor(data$State)
+data$FWI_90th_flag <- as.factor(data$FWI_90th_flag)
+data$FWI_99th_flag <- as.factor(data$FWI_99th_flag)
+data$Bushfire_Flag <- as.factor(data$Bushfire_Flag)
+
+## Compare different thresholds for FWI flag against Bushfire flag
+### A.90th percentile
+ggplot(data=data)+
+  geom_bar(mapping=aes(x=Bushfire_Flag,fill=FWI_90th_flag),
+           position="fill")+
+  facet_wrap(~State,nrow=3)+
+  scale_fill_manual(values=c("#008B92","#FFE781"))+
+  theme(panel.background = element_rect(fill="white"))+
+  ggtitle("FWI 90th Percentile Threshold", "Higher False Positive")
+
+### B.99th percentile
+ggplot(data=data)+
+  geom_bar(mapping=aes(x=Bushfire_Flag,fill=FWI_99th_flag),
+           position="fill")+
+  facet_wrap(~State,nrow=3)+
+  scale_fill_manual(values=c("#008B92","#FFE781"))+
+  theme(panel.background = element_rect(fill="white"))+
+  ggtitle("FWI 99th Percentile Threshold", "Higher False Negative")
+
+# use 90th percentile as threshold since there is preference for higher false positive than false negative
+data <- data %>% 
+  select(-c("FWI_90th","FWI_99th","FWI_max","FWI_99th_flag"))
+
+## Add flags for SOI and IOD
+data <- data %>% 
+  mutate(SOI_Condition = case_when(SOI <= 7 & SOI >= -7 ~ "Normal SOI",
+                                   SOI > 7 ~ "El Nino",
+                                   SOI < -7 ~ "La Nina")) %>%
+  mutate(IOD_Phase = case_when(IOD <= 0.4 & IOD >= -0.4 ~ "Normal IOD",
+                               IOD > 0.4 ~ "Positive",
+                               IOD < -0.4 ~ "Negative")) %>% 
+  select(-c("IOD", "SOI"))
+
+# Format as factor
+data$SOI_Condition <- as.factor(data$SOI_Condition)
+data$IOD_Phase <- as.factor(data$IOD_Phase)
+
+# Set baseline
+data <- within(data, SOI_Condition <- relevel(SOI_Condition, ref = "Normal SOI"))
+data <- within(data, IOD_Phase <- relevel(IOD_Phase, ref = "Normal IOD"))
+
+## Compare bushfire to different SOI conditions and IOD phases
+### A.SOI condition
+ggplot(data=data)+
+  geom_bar(mapping=aes(x=Bushfire_Flag,fill=SOI_Condition),
+           position="fill")+
+  facet_wrap(~State,nrow=3)+
+  theme(panel.background = element_rect(fill="white"))+
+  scale_fill_manual(values=c("#F9F9F9","#008B92","#FFE781"))+
+  ggtitle("SOI Condition")
+
+### B.IOD phase
+ggplot(data=data)+
+  geom_bar(mapping=aes(x=Bushfire_Flag,fill=IOD_Phase),
+           position="fill")+
+  facet_wrap(~State,nrow=3)+
+  theme(panel.background = element_rect(fill="white"))+
+  scale_fill_manual(values=c("#F9F9F9","#008B92","#FFE781"))+
+  ggtitle("IOD Phase")
+
